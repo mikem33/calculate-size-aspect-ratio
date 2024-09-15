9084513a2c1f7a4a3b106e623d4e98a6751e968c
Miguel Morera <info@miguelmorera.com>
2024-09-15 09:03:16 +0200

    Base layout and javascript logic made

--- source/assets/javascript/compile/main.js
+++ source/assets/javascript/compile/main.js
@@ -1,1 +1,55 @@
-console.log('Hello world! This is Pandora Boilerplate.');
+function calculateAspectRatio(width, height, desiredWidth = null, desiredHeight = null) {
+    
+    if (isNaN(width) || isNaN(height) || isNaN(desiredWidth) || isNaN(desiredHeight)) {
+        return { status: 0, message: 'A valid number is required.'}
+    }
+    width = width;
+    height = height;
+
+    const aspectRatio = width / height;
+
+    if (desiredWidth && desiredHeight) {
+        return { status: 0, message: 'Introduce only one measure.' };
+    }
+    
+    if (desiredWidth !== null && desiredWidth !== '') {
+        
+        const calculatedHeight = desiredWidth / aspectRatio;
+        return { status: 1, width: desiredWidth, height: Math.round(calculatedHeight) };
+    }
+
+    if (desiredHeight !== null && desiredHeight !== '') {
+        
+        const calculatedWidth = desiredHeight * aspectRatio;
+        return { status: 1, width: Math.round(calculatedWidth), height: desiredHeight };
+    }
+
+    return null;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const inputs = document.querySelectorAll('input[type="number"]');
+
+    inputs.forEach(input => {            
+        input.addEventListener('keyup', () => {
+            const params = [
+                document.getElementById('width').value,
+                document.getElementById('height').value,
+                document.getElementById('new-width').value,
+                document.getElementById('new-height').value
+            ];
+
+            const result = calculateAspectRatio(...params);
+            const outputWrapper = document.getElementById('result');
+            if (result === null) {
+                outputWrapper.innerHTML = '';
+            } else {
+                if (result.status == 0) {
+                    outputWrapper.innerHTML = `${result.message}`;
+                } else {
+                    outputWrapper.innerHTML = `${result.width}x${result.height}`;
+                }
+            }
+        });
+    });
+});
