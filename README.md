fa30bf4afe1c9dd058063e24a27a28e716dc0b0a
Miguel Morera <info@miguelmorera.com>
2024-09-15 06:52:40 +0200

    Initial boilerplate files

---
+++ README.md
@@ -0,0 +1,64 @@
+# calculate-size-aspect-ratio
+Given a size, calculate the increase or decrease of the size maintaining the aspect-ratio
+
+I am using [Stylus](http://stylus-lang.com) for preprocessing _CSS_.
+
+The first thing you have to do to start to work is install _NPM_ dependencies:
+```
+npm install --save
+```
+This will install all the necessary stuff to start to work. 
+```javascript
+var del         = require('del'),
+    gulp        = require('gulp'),
+    watch       = require('gulp-watch'),
+    stylus      = require('gulp-stylus'),
+    rename      = require("gulp-rename"),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    notify      = require("gulp-notify"),
+    realFavicon = require('gulp-real-favicon'),
+    runSequence = require('gulp4-run-sequence');
+```
+
+> The module [gulp-notify](https://github.com/mikaelbr/gulp-notify) is totally optional. I found it very useful to get notifications when I'm developing a project.
+
+## Developing
+When we finish the previous setup we can start our work in the "source" folder.
+
+The tasks you can use for developing your project are basically **CSS compiling and javascript files concatenation**. We have to use the command `gulp watch` for watch changes in the files that we are working on. This task will watch if there are any changes in any of the `.styl` files in the _CSS_ folder and also the changes in any of the custom javascript files.
+
+When a change is detected in these files the compiling task (in the case of _CSS_) or the concatenation task (in the case of _JS_) will be launched.
+
+```javascript
+// Compile Stylus CSS
+gulp.task('style', function (done) {
+    gulp.src('source/assets/css/compile/styl/style.styl')
+        .pipe(stylus({
+            'include css': true
+        }))
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('source/assets/css'))
+        .pipe(notify('CSS Compiled!'));
+    done();
+});
+
+// Generate Javascript
+gulp.task('js', function(done){
+    return gulp.src(['source/assets/javascript/compile/*.js'])
+        .pipe(concat('javascript.js'))
+        .pipe(gulp.dest('source/assets/javascript'))
+        .pipe(notify('JS Compiled!'))
+        .pipe(uglify())
+        .pipe(gulp.dest('source/assets/javascript'));
+    done();
+});
+
+// Watch
+gulp.task('watch', function() {
+    gulp.watch('source/assets/javascript/compile/*.js', gulp.series('js'));
+    gulp.watch('source/assets/css/compile/styl/*.styl', gulp.series('style'));
+});
+```
+
+When we finish our work, we will use `gulp build` to generate the final project that we are using as production site. This task will copy to the `build` folder all the necessary files to deploy the project.
---
+++ gulpfile.js
@@ -0,0 +1,131 @@
+var del         = require('del'),
+    gulp        = require('gulp'),
+    watch       = require('gulp-watch'),
+    stylus      = require('gulp-stylus'),
+    rename      = require("gulp-rename"),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    notify      = require("gulp-notify"),    
+    realFavicon = require('gulp-real-favicon'),
+    runSequence = require('gulp4-run-sequence');
+
+// Compile Stylus CSS
+gulp.task('style', function (done) {
+    gulp.src('source/assets/css/compile/styl/style.styl')
+        .pipe(stylus({
+            compress : true,
+            'include css': true
+        }))
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('source/assets/css'))
+        .pipe(notify('CSS Compiled!'));
+    done();
+});
+
+// Generate Javascript
+gulp.task('js', function(done){
+    return gulp.src(['source/assets/javascript/compile/*.js'])
+        .pipe(concat('javascript.js'))
+        .pipe(gulp.dest('source/assets/javascript'))
+        .pipe(notify('JS Compiled!'))
+        .pipe(uglify())
+        .pipe(gulp.dest('source/assets/javascript'));
+    done();
+});
+
+// Watch
+gulp.task('watch', function() {
+    gulp.watch('source/assets/javascript/compile/*.js', gulp.series('js'));
+    gulp.watch('source/assets/css/compile/styl/*.styl', gulp.series('style'));
+});
+
+gulp.task('generate-favicon', function(done) {
+    // File where the favicon markups are stored (unnecessary but I don't know how to avoid its generation).
+    var FAVICON_DATA_FILE = 'source/assets/images/favicons/faviconData.json';
+    realFavicon.generateFavicon({
+        masterPicture: 'source/assets/images/favicons/favicon-master.png',
+        dest: 'source/assets/images/favicons',
+        iconsPath: 'source/assets/images/favicons/',
+        design: {
+            ios: {
+                pictureAspect: 'noChange',
+                assets: {
+                    ios6AndPriorIcons: false,
+                    ios7AndLaterIcons: false,
+                    precomposedIcons: false,
+                    declareOnlyDefaultIcon: true
+                }
+            },
+            desktopBrowser: {},
+            windows: {
+                pictureAspect: 'noChange',
+                backgroundColor: '#2b5797',
+                onConflict: 'override',
+                assets: {
+                    windows80Ie10Tile: false,
+                    windows10Ie11EdgeTiles: {
+                        small: false,
+                        medium: true,
+                        big: false,
+                        rectangle: false
+                    }
+                }
+            },
+            androidChrome: {
+                pictureAspect: 'noChange',
+                themeColor: '#ffffff',
+                manifest: {
+                    display: 'standalone',
+                    orientation: 'notSet',
+                    onConflict: 'override',
+                    declared: true
+                },
+                assets: {
+                    legacyIcon: false,
+                    lowResolutionIcons: false
+                }
+            },
+            safariPinnedTab: {
+                pictureAspect: 'blackAndWhite',
+                threshold: 50,
+                themeColor: '#5bbad5'
+            }
+        },
+        settings: {
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: false,
+            usePathAsIs: false
+        },
+        markupFile: FAVICON_DATA_FILE
+    }, function() {
+        done();
+    });
+});
+
+gulp.task('cleanBuild', function () {
+    return del('build');
+});
+
+gulp.task('copy', function(done) {
+    gulp.src('source/*.html')
+        .pipe(gulp.dest('build/'))
+    gulp.src('source/assets/css/style.css')
+        .pipe(gulp.dest('build/assets/css/'))
+    gulp.src('source/assets/javascript/*.js')
+        .pipe(gulp.dest('build/assets/javascript/'))
+    gulp.src('source/assets/images/*')
+        .pipe(gulp.dest('build/assets/images/'))
+    gulp.src('source/assets/images/*/*')
+        .pipe(gulp.dest('build/assets/images/'))
+        .pipe(notify('All the resources has been copied to the build folder.'));
+    done();
+});
+
+gulp.task('build', function(callback) {
+    runSequence('cleanBuild','style', 'js', 'copy', callback);
+});
+
+// Default gulp task to run 
+gulp.task('default', gulp.series('watch'));
